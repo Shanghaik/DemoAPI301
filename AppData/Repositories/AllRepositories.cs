74841f1e0fb6a4c624be1a5dd875e6e508be68a2
shanghaik168@gmail.com <Shanghaik1998>
2023-05-20 15:24:48 +0700

    repo

---
+++ AppData/Repositories/AllRepositories.cs
@@ -0,0 +1,67 @@
+﻿using AppData.IRepositories;
+using AppData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppData.Repositories
+{
+    public class AllRepositories<T> : IAllRepositories<T> where T : class
+    {
+        FINALASS_FPOLYSHOP_FA22_SOF205__SOF2041Context context;
+        DbSet<T> dbset;
+        public AllRepositories() { }
+
+        public AllRepositories(FINALASS_FPOLYSHOP_FA22_SOF205__SOF2041Context context, DbSet<T> dbset)
+        {
+            this.context = context;
+            this.dbset = dbset;
+        }
+
+        public bool CreateItem(T item)
+        {
+            try
+            {
+                dbset.Add(item);
+                context.SaveChanges(); return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteItem(T item)
+        {
+            try
+            {
+                dbset.Remove(item);
+                context.SaveChanges(); return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return dbset.ToList();
+        }
+
+        public bool UpdateItem(T item)
+        {
+            try
+            {
+                dbset.Update(item);
+                context.SaveChanges(); return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
